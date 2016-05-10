bde9957aa85d73f02aa55b3ccdfd4c2d2df01d74
Alex Ellis <alexellis2@gmail.com>
2016-05-10 22:01:15 +0100

    add pihut

--- stock_fetch/index.js
+++ stock_fetch/index.js
@@ -9,18 +9,43 @@ function main() {
 
   subscribe.on("message", (channel, message) => {
     if(channel == "stock") {
-      if(message == "pimoroni") {
+
+      if(message == "pihut") {
+        push.get("pihut.stock", (err, pihutstock)=> {
+          if(!pihutstock) {
+
+            const collectionUrl = "https://thepihut.com/products/raspberry-pi-zero?variant=14062715972";
+            request.get({url: collectionUrl, "User-Agent": "pi-check"}, (err, response, body) => {
+              var lines = body.split("\n");
+              lines.forEach(function(line) {
+                if(line.indexOf("product: {") >- 1) {
+                  let processLine = line.replace("product", "\"product\"").trim();
+
+                  processLine = "{" + processLine.substring(0,processLine.length-1) + "}";
+                  let parsed = JSON.parse(processLine);
+                  var found = false;
+                  parsed.product.variants.forEach(function(variant) {
+                    if(variant.inventory_quantity) {
+                      found = true;
+                    }
+                  });
+                  push.set("pihut.stock", found, (err) => {
+                    push.expire("pihut.stock", 2, (err) => {
+
+                    });
+                  });
+                }
+              });
+            });
+          }
+        });
+      }
+      else if(message == "pimoroni") {
 
         push.get("pimoroni.stock", (err, value) => {
           if(!value) {
-            console.log("Fetching pimoroni stock")
-
             var collectionUrl = "https://shop.pimoroni.com/collections/raspberry-pi";
             request.get({url: collectionUrl, "User-Agent": "pi-check"}, (err, response, body) => {
-              push.set("pimoroni.access", new Date().getTime(), (err) => {
-                if(err) {
-                  return console.error(err);
-                }
                 let stock = body.indexOf("stock-level in-stock") >=0;
                 push.set("pimoroni.stock", stock, (err) => {
                   if(err) {
@@ -32,7 +57,6 @@ function main() {
                     }
                   })
                 });
-              });
             });
           }
         });
