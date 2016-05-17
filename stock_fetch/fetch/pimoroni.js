c24430d2467afd035ac53027abbac3edcd49d8d0
Alex Ellis <alexellis2@gmail.com>
2016-05-17 15:40:38 +0100

    Update pimoroni.js

--- stock_fetch/fetch/pimoroni.js
+++ stock_fetch/fetch/pimoroni.js
@@ -2,30 +2,46 @@
 
 var request = require('request');
 
-module.exports = class PimoroniScrape {
+class PimoroniScrape {
   constructor() {
     this.productPage = "https://shop.pimoroni.com/products/raspberry-pi-zero.js";
   }
 
-  refresh(done) {
-    request.get({url: this.productPage, "User-Agent": "pi-check", "json": true}, (err, response, body) => {
-      let stock = {};
-      let found = false;
-      let total = 0;
-      try {
-        body.variants.forEach((v) => {
-           if(v.inventory_quantity) {
-             let val = Number(v.inventory_quantity);
-             if(val > 0) {
-                total += val;
+  _pullCounts(url) {
+    return new Promise((resolve, reject) => {
+      request.get({url: url, "User-Agent": "pi-check", "json": true}, (err, response, body) => {
+        let total = 0;
+        try {
+          body.variants.forEach((v) => {
+             if(v.inventory_quantity) {
+               let val = Number(v.inventory_quantity);
+               if(val > 0) {
+                  total += val;
+               }
              }
-           }
-        });
-      } catch(e) {
-        console.error(e);
-      }
-      done(err, {stock: total>0, totalAmount: total});
+          });
+        } catch(e) {
+          console.error(e);
+          resolve(0);
+        }
+        resolve(total);
+      });
+    });
+  }
+
+  refresh(done) {
+    var stock = {};
+    var total = 0;
+    Promise.all([
+      this._pullCounts(this.productPage ),
+      this._pullCounts("https://shop.pimoroni.com/products/pi-zero-complete-starter-kit.js"),
+      this._pullCounts("https://shop.pimoroni.com/products/pi-zero-project-kits.js")
+    ]).then((results) => {
+      results.forEach((r)=> {
+        total+=r;
+      });
+      done(null, {stock: total>0, totalAmount: total});
     });
   }
 }
-
+module.exports = PimoroniScrape;
